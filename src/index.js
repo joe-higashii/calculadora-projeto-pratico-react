e9d9334b7883d8f320af9889fe1b239a96116cf8
Joe <joedsonfera@hotmail.com>
2023-07-28 16:00:55 -0300

    second commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom";
 import App from "./components/App";
 
-import './index.css';
+import "./index.css";
 
-
-ReactDOM.createRoot(document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
